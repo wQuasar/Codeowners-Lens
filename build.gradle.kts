2c70278f0df86792caf84203df601354ed300b0c
Shaishav Saraswat <shaishav.saraswat@zalando.de>
2024-03-10 23:00:08 +0100

    define version in libs.versions.toml file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,53 @@
 plugins {
-  id("java")
-  id("org.jetbrains.kotlin.jvm") version "1.9.21"
-  id("org.jetbrains.intellij") version "1.16.1"
+    id("java")
+    alias(libs.plugins.jetBrainsKotlin)
+    alias(libs.plugins.jetBrainsIntellij)
+    alias(libs.plugins.kotlinKapt)
 }
 
 group = "com.wquasar"
 version = "1.0-SNAPSHOT"
 
 repositories {
-  mavenCentral()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.dagger.get())
+    kapt(libs.daggerKapt.get())
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2023.1.5")
-  type.set("IC") // Target IDE Platform
+    version.set(libs.versions.intellijPlugin.get())
+    type.set("IC") // Target IDE Platform
 
-  plugins.set(listOf(/* Plugin Dependencies */))
+    plugins.set(listOf(/* Plugin Dependencies */))
 }
 
 tasks {
-  // Set the JVM compatibility versions
-  withType<JavaCompile> {
-    sourceCompatibility = "17"
-    targetCompatibility = "17"
-  }
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
-  }
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = libs.versions.jvmVersion.get()
+        targetCompatibility = libs.versions.jvmVersion.get()
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = libs.versions.jvmVersion.get()
+    }
 
-  patchPluginXml {
-    sinceBuild.set("231")
-    untilBuild.set("241.*")
-  }
+    patchPluginXml {
+        sinceBuild.set(libs.versions.sinceBuild.get())
+        untilBuild.set(libs.versions.untilBuild.get())
+    }
 
-  signPlugin {
-    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-    privateKey.set(System.getenv("PRIVATE_KEY"))
-    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-  }
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
 
-  publishPlugin {
-    token.set(System.getenv("PUBLISH_TOKEN"))
-  }
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
 }
