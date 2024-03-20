c4315a6a2526f0dc2928491f5f806f4ad9e48cd4
Shaishav Saraswat <shaishav.saraswat@zalando.de>
2024-03-20 18:20:31 +0100

    show balloon popup for no codeowners, fix popup position in commit action

--- src/main/kotlin/com/wquasar/codeowners/visibility/commit/CodeOwnersCommitAction.kt
+++ src/main/kotlin/com/wquasar/codeowners/visibility/commit/CodeOwnersCommitAction.kt
@@ -3,14 +3,20 @@ package com.wquasar.codeowners.visibility.commit
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.MessageType
+import com.intellij.openapi.ui.popup.Balloon
+import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.vcs.ProjectLevelVcsManager
 import com.intellij.openapi.vcs.changes.ChangeListManager
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.awt.RelativePoint
 import com.wquasar.codeowners.visibility.core.CodeOwnerService
 import com.wquasar.codeowners.visibility.di.CodeOwnersComponentProvider
 import com.wquasar.codeowners.visibility.file.FilesHelper
+import java.awt.Point
 import java.awt.event.MouseEvent
 import javax.inject.Inject
+import javax.swing.JComponent
 
 internal class CodeOwnersCommitAction : AnAction() {
 
@@ -37,7 +43,25 @@ internal class CodeOwnersCommitAction : AnAction() {
 
     override fun actionPerformed(actionEvent: AnActionEvent) {
         val codeOwnersMap = populateCodeOwnersMap()
-        createAndShowPopup(codeOwnersMap, actionEvent)
+        if (codeOwnersMap.isEmpty()) {
+            createAndShowEmptyCodeownersPopup(actionEvent)
+        } else {
+            createAndShowCodeownersPopup(codeOwnersMap, actionEvent)
+        }
+    }
+
+    private fun createAndShowEmptyCodeownersPopup(actionEvent: AnActionEvent) {
+        val displayPoint = getPopupDisplayPoint(actionEvent) ?: return
+        val balloon = JBPopupFactory.getInstance()
+            .createHtmlTextBalloonBuilder("No code owners found", MessageType.INFO, null)
+            .setFadeoutTime(5000)
+            .createBalloon()
+        balloon.show(displayPoint, Balloon.Position.below)
+    }
+
+    private fun getPopupDisplayPoint(actionEvent: AnActionEvent): RelativePoint? {
+        val component = actionEvent.inputEvent.component as? JComponent ?: return null
+        return RelativePoint(component, Point(component.width / 2, component.height))
     }
 
     private fun isGitEnabled(): Boolean {
@@ -62,13 +86,13 @@ internal class CodeOwnersCommitAction : AnAction() {
         return codeOwnerMap
     }
 
-    private fun createAndShowPopup(
+    private fun createAndShowCodeownersPopup(
         codeOwnerMap: HashMap<String, MutableList<VirtualFile>>,
         actionEvent: AnActionEvent,
     ) {
         val mouseEvent = actionEvent.inputEvent as? MouseEvent
         val point = mouseEvent?.point ?: return
-        val component = actionEvent.getData(PlatformDataKeys.CONTEXT_COMPONENT) ?: return
+        val component = actionEvent.inputEvent.component as? JComponent ?: return
 
         val actionGroup = DefaultActionGroup().apply {
             codeOwnerMap.keys.forEach { owner ->
