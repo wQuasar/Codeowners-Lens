1d7b27a2010054898435a3962ec2d10da9db0f65
Shaishav Saraswat <shaishavsaraswat@zoho.com>
2024-08-29 21:43:46 +0200

    Support Intellij Plugin 2.x (#3)
    * move to intellij platform plugin 2.x
    
    * remove intellij platform migration plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 plugins {
     id("java")
     alias(libs.plugins.jetBrainsKotlin)
@@ -6,49 +9,71 @@ plugins {
     alias(libs.plugins.detekt)
 }
 
-group = "com.wquasar"
-version = "0.6.1"
 
 repositories {
     mavenCentral()
-}
-
-intellij {
-    version.set(libs.versions.intellijPlugin.get())
-    type.set("IC") // Target IDE Platform
-
-    plugins.set(listOf(/* Plugin Dependencies */))
-}
-
-tasks {
-    withType<JavaCompile> {
-        sourceCompatibility = libs.versions.jvmVersion.get()
-        targetCompatibility = libs.versions.jvmVersion.get()
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = libs.versions.jvmVersion.get()
-    }
-
-    patchPluginXml {
-        sinceBuild.set(libs.versions.sinceBuild.get())
-        untilBuild.set(libs.versions.untilBuild.get())
-    }
-
-    signPlugin {
-        certificateChain.set(providers.environmentVariable("CERTIFICATE_CHAIN"))
-        privateKey.set(providers.environmentVariable("PRIVATE_KEY"))
-        password.set(providers.environmentVariable("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(providers.environmentVariable("PUBLISH_TOKEN"))
+    intellijPlatform {
+        defaultRepositories()
+        intellijDependencies()
     }
 }
 
 dependencies {
+    intellijPlatform {
+        create("IC", libs.versions.intellijPlugin.get())
+        instrumentationTools()
+        zipSigner()
+    }
+
+    testImplementation(kotlin("test"))
     implementation(libs.dagger.get())
     kapt(libs.daggerKapt.get())
 
     testImplementation(libs.mockito.get())
     testImplementation(libs.mockitoKotlin.get())
+    testImplementation(libs.mockitoInline.get())
+}
+
+intellijPlatform {
+    buildSearchableOptions = true
+    instrumentCode = true
+    projectName = project.name
+
+    group = "com.wquasar"
+    version = "0.6.2"
+
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = libs.versions.sinceBuild.get()
+            untilBuild = libs.versions.untilBuild.get()
+        }
+    }
+
+    signing {
+        certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+        privateKey = providers.environmentVariable("PRIVATE_KEY")
+        password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+    }
+
+    publishing {
+        token = providers.environmentVariable("PUBLISH_TOKEN")
+    }
+}
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = JvmTarget.JVM_17.target
+        targetCompatibility = JvmTarget.JVM_17.target
+    }
+
+    withType<KotlinJvmCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+            freeCompilerArgs.add("-opt-in=kotlin.RequiresOptIn")
+        }
+    }
+
+    test {
+        useJUnit()
+    }
 }
